72fe5bf5332ae73485a662eec4ca894fc919bb16
Usman <umahmood@gmail.com>
2021-01-26 23:47:11 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,34 +2,152 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Car from './AppCar.js';
 import reportWebVitals from './reportWebVitals';
 
-class Car {
-	constructor(name){
-		this.brand = name;
+// class Car {
+// 	constructor(name){
+// 		this.brand = name;
+// 	}
+
+// 	present() {
+// 		return 'I have a '+ this.brand;
+// 	}
+// }
+
+// class Model extends Car {
+// 	constructor(name,mod){
+// 		super(name);
+// 		this.model = mod;
+// 	}
+
+// 	show(){
+// 		return this.present() + ', it is a '+ this.model;
+// 	}
+// }
+
+// var mycar = new Model("Honda","Accord");
+
+// const myfirstelement = mycar.show();
+
+// const myelement = (
+// 	<div>
+// 		<h1>I am a Header</h1>
+// 		<h1>I am a Header Too.</h1>
+// 	</div>
+// 	);
+
+// // const myelement = <h1>React is {5 + 5} times better with JSX</h1>;
+
+// ReactDOM.render(myelement,document.getElementById('root'));
+
+// class Car extends React.Component {
+//    render() {
+//     return <h2>I am a Car!</h2>;
+//   }
+// }
+
+// class Garage extends React.Component {
+// 	render() {
+// 		return (
+// 		<div>
+// 		<h1>Who lives in my Garage?</h1>
+// 		<Car />
+// 		</div>
+// 		);
+// 	}
+// }
+
+
+// class Car extends React.Component {
+// 	constructor(props){
+// 		super(props);
+// 		this.state = {
+// 			brand: "Ford",
+// 			model: "Mustag",
+// 			color: "red",
+// 			year: 1964
+// 		};
+// 	}
+
+// 	changeColor = () => {
+// 		this.setState({color:"blue"});
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 			<h1>My {this.state.brand}</h1>
+// 			<p>
+// 			It is a {this.state.color} {this.state.model} 
+// 			from {this.state.year}.
+// 			</p>
+// 			<button
+// 			type="button"
+// 			onClick={this.changeColor}
+// 			>Change color</button>
+// 			</div>
+// 			);
+// 	}
+// }
+
+// class Garage extends React.Component {
+// 	render() {
+// 		const carinfo = {name: "Ford",model: "Mustang"};
+// 		return (
+// 		<div>
+// 		<h1>Who lives in my Garage?</h1>
+// 		<Car brand={carinfo['name'] + ' ' + carinfo['model']} />
+// 		</div>
+// 		);
+// 	}
+// }
+
+class Container extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {show: true};
 	}
 
-	present() {
-		return 'I have a '+ this.brand;
+	delHeader = () => {
+		this.setState({show: false});
+	}
+
+	render() {
+		let myheader;
+		if(this.state.show){
+			myheader = <Child />;
+		};
+		return (
+			<div>
+			{myheader}
+			<button type="button" onClick={this.delHeader}>Delete Header</button>
+			</div>
+		);
+	}
+
+}
+
+
+class Child extends React.Component{
+	componentWillUnmount(){
+		alert('The component named Header is about to be unmounted');
+	}
+	render(){
+		return (
+		<h1>Hello World</h1>
+		);
 	}
 }
 
-class Model extends Car {
-	constructor(name,mod){
-		super(name);
-		this.model = mod;
-	}
 
-	show(){
-		return this.present() + ', it is a '+ this.model;
-	}
-}
 
-var mycar = new Model("Honda","Accord");
 
-const myfirstelement = mycar.show();
+ReactDOM.render(<Container />,document.getElementById('root'));
 
-ReactDOM.render(myfirstelement,document.getElementById('root'));
+
+
+
 
 
 
